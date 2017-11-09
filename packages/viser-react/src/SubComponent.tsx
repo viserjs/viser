effe379b660757c36493b02c58aedb2188cc25e8
arcthur <arthurtemptation@gmail.com>
2017-11-09 21:55:25 +0800

    feat: update viser-react

---
+++ packages/viser-react/src/SubComponent.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+
+class Props {}
+
+export default class SubComponent extends React.Component<Props, any> {
+  static contextTypes = {
+    centralizedUpdates: PropTypes.func,
+    hasInViews: PropTypes.bool,
+    viewId: PropTypes.number,
+  };
+
+  displayName = 'SubComponent';
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  componentDidUpdate() {
+    this.context.centralizedUpdates(this);
+  }
+
+  componentDidMount() {
+    this.context.centralizedUpdates(this);
+  }
+
+  render() {
+    return null;
+  }
+}
