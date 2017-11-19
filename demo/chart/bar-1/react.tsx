93b87fb51276176ff579d393533277e9481fe3d9
arcthur <arthurtemptation@gmail.com>
2017-11-19 13:21:46 +0800

    demo: update status

--- demo/chart/bar-1/react.tsx
+++ demo/chart/bar-1/react.tsx
@@ -4,19 +4,32 @@ import * as React from 'react';
 import { data, dataMapping, dataPre, scale } from './data'
 
 class App extends React.Component {
+  state = {
+    height: 400,
+    lineWidth: 1
+  }
+
   constructor(props) {
     super(props);
+  }
+
+  handleClick = () => {
+    this.setState({
+      height: 600,
+      lineWidth: 10,
+    })
   }
 
   render() {
     return (
       <div>
-        {/* <Chart forceFit height={400} data={data} dataPre={dataPre} dataMapping={dataMapping} scale={scale}>
+        <button onClick={this.handleClick}>Click</button>
+        <Chart forceFit height={this.state.height} data={data} dataPre={dataPre} dataMapping={dataMapping} scale={scale}>
           <Tooltip />
           <Axis />
-          <StackBar style={{ stroke: '#fff', lineWidth: 1 }} />
-        </Chart> */}
-        <StackBarChart height={400} data={data} dataPre={dataPre} dataMapping={dataMapping} forceFit scale={scale} tooltip axis legend />
+          <StackBar style={{ stroke: '#fff', lineWidth: this.state.lineWidth }} />
+        </Chart>
+        {/* <StackBarChart height={400} data={data} dataPre={dataPre} dataMapping={dataMapping} forceFit scale={scale} tooltip axis legend /> */}
       </div>
     );
   }
