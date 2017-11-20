446b4cb7c3a84d4539c0398c19a6f3e676293004
arcthur <arthurtemptation@gmail.com>
2017-11-20 16:10:51 +0800

    fix: update demo to use new function

--- demo/chart/bar-1/react.tsx
+++ demo/chart/bar-1/react.tsx
@@ -29,8 +29,7 @@ class App extends React.Component {
           <Axis />
           <StackBar style={{ stroke: '#fff', lineWidth: this.state.lineWidth }} />
         </Chart>
-        {/* <StackBarChart height={400} data={data} dataPre={dataPre} dataMapping={dataMapping} forceFit scale={scale} tooltip axis legend /> */}
-        <LiteChart gemo="stackBar" height={400} data={data} dataMapping={dataMapping} forceFit />
+        {/* <LiteChart height={400} data={data} dataPre={dataPre} dataMapping={dataMapping} forceFit stackBar /> */}
       </div>
     );
   }
