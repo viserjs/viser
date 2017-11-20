16ad464a81669b317366c43b7f00739d9595bd63
ludy.hxy <ludy.hxy@alibaba-inc.com>
2017-11-20 17:26:38 +0800

    feat: remove ununseless file

--- viser/src/index.ts
+++
@@ -1,8 +0,0 @@
-import CommonChart from './core/CommonChart';
-
-export default function(config: any) {
-  const commonChart = new CommonChart(config);
-  commonChart.render();
-
-  return commonChart;
-}
