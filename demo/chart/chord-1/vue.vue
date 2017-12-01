4ae1dbc58d5fdf228f5d0ffc882d8bff1f002c1b
arcthur <arthurtemptation@gmail.com>
2017-12-01 17:01:06 +0800

    feat: add vue chord demo

---
+++ demo/chart/chord-1/vue.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-chart :force-fit="true" :height="600" :data-pre="dataPre" :data="data" :scale="scale">
+      <v-view :view-id="'2'" :data-view="'edges'">
+        <v-coord :type="'polar'" :direction="'yReverse'" />
+        <v-edge :position="'x*y'" :color="'source'" :shape="'arc'" :opacity="0.5" :tooltip="'source*target*value'" />
+      </v-view>
+      <v-view :view-id="'3'" :data-view="'nodes'">
+        <v-coord :type="'polar'" :direction="'yReverse'" />
+        <v-polygon :position="'x*y'" :color="'id'" :label="label" />
+      </v-view>
+    </v-chart>
+  </div>
+</template>
+<script>
+import { data, scale, dataPre } from "./data";
+
+export default {
+  data() {
+    return {
+      data,
+      dataPre,
+      scale,
+      label: [
+        'name', {
+          labelEmit: true,
+          textStyle: {
+            fill: '#8c8c8c'
+          },
+        }
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
