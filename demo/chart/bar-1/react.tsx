bf6b9385eea0f7d6d13844cb69ca1db48ac22c25
evilucifero <x@evilucifero.com>
2017-12-21 15:33:13 +0800

    fix: rerender dataview bug

--- demo/chart/bar-1/react.tsx
+++ demo/chart/bar-1/react.tsx
@@ -3,10 +3,30 @@ import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import { data, dataPre, scale } from './data'
 
+export const repaintData = [
+  { country: 'Europe', year: '1750', value: 2163 },
+  { country: 'Europe', year: '1800', value: 2203 },
+  { country: 'Europe', year: '1850', value: 2276 },
+  { country: 'Europe', year: '1900', value: 2408 },
+  { country: 'Europe', year: '1950', value: 2547 },
+  { country: 'Europe', year: '1999', value: 2729 },
+  { country: 'Europe', year: '2050', value: 2628 },
+  { country: 'Europe', year: '2100', value: 2828 },
+  { country: 'Asia', year: '1750', value: 2502 },
+  { country: 'Asia', year: '1800', value: 2635 },
+  { country: 'Asia', year: '1850', value: 2809 },
+  { country: 'Asia', year: '1900', value: 2947 },
+  { country: 'Asia', year: '1950', value: 21402 },
+  { country: 'Asia', year: '1999', value: 23634 },
+  { country: 'Asia', year: '2050', value: 25268 },
+  { country: 'Asia', year: '2100', value: 27268 }
+];
+
 class App extends React.Component {
   state = {
     height: 400,
-    lineWidth: 1
+    lineWidth: 1,
+    data,
   }
 
   constructor(props) {
@@ -17,11 +37,12 @@ class App extends React.Component {
     this.setState({
       height: 600,
       lineWidth: 10,
+      data: repaintData,
     })
   }
 
   render() {
-
+    const { data } = this.state;
     return (
       <div>
         <button onClick={this.handleClick}>Click</button>
