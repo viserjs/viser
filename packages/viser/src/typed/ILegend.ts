f24ef22245b5e4dab0029170f645b9c775eb590f
arcthur <arthurtemptation@gmail.com>
2017-12-25 15:02:37 +0800

    fix: fix typed

--- packages/viser/src/typed/ILegend.ts
+++ packages/viser/src/typed/ILegend.ts
@@ -9,6 +9,7 @@ export interface ILegend {
   show?: boolean;
   position?: 'top' | 'right' | 'bottom' | 'left';
   title?: null | object;
+  custom?: boolean;
   offset?: number;
   offsetX?: number;
   offsetY?: number;
