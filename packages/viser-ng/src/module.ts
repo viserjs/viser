6dafcf09e4dcc22cbeef2ea44ac0ca9313a724bb
ludy.hxy <huxiaoyunhust@gmail.com>
2018-01-05 14:49:21 +0800

    feat: viser-ng add StackInterval

--- packages/viser-ng/src/module.ts
+++ packages/viser-ng/src/module.ts
@@ -2,7 +2,7 @@ import { NgModule, enableProdMode } from '@angular/core';
 import { Chart } from './Chart';
 import { LiteChart } from './LiteChart';
 import { Axis, Coord, Facet, Guide, Legend, Tooltip, View, FacetView, Series, Pie, Sector, Line, SmoothLine, DashLine, Area, StackArea, SmoothArea,
- Bar, StackBar, DodgeBar, Point, Funnel, Pyramid, Schema, Box, Candle, Polygon, Contour, Heatmap, Edge, Sankey, ErrorBar, JitterPoint } from './components/index';
+ Bar, StackBar, DodgeBar, Point, Funnel, Pyramid, Schema, Box, Candle, Polygon, Contour, Heatmap, Edge, Sankey, ErrorBar, JitterPoint, StackInterval } from './components/index';
 import * as viser from 'viser';
 
 @NgModule({
@@ -42,6 +42,7 @@ import * as viser from 'viser';
     Sankey,
     ErrorBar,
     JitterPoint,
+    StackInterval,
   ],
   exports: [
     Chart,
@@ -79,6 +80,7 @@ import * as viser from 'viser';
     Sankey,
     ErrorBar,
     JitterPoint,
+    StackInterval,
   ],
 })
 export class ViserModule {
