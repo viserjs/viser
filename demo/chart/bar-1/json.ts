68b6ac8cc8c709bfa49037d4391c31394e72763b
arcthur <arthurtemptation@gmail.com>
2018-01-09 14:49:36 +0800

    fix: add demo

--- demo/chart/bar-1/json.ts
+++ demo/chart/bar-1/json.ts
@@ -2,87 +2,32 @@ import viser from '../../../packages/viser/src/index';
 import { data, scale } from './data'
 const DataSet = require('@antv/data-set');
 
-const ds: any = new DataSet();
-const dv = ds.createView().source(data);
-
-dv.transform({
-  type: 'percent',
-  field: 'value',
-  dimension: 'country',
-  groupBy: ['year'],
-  as: 'percent'
-});
+const ds = new DataSet();
+const dv = ds.createView('test')
+  .source(data)
+  .transform({
+    as: [ 'count' ],
+    groupBy: [ 'release' ],
+    operations: [ 'count' ],
+    type: 'aggregate'
+  });
 
 viser({
-  data: dv.rows,
+  data: dv,
   scale: scale,
   axis: true,
   tooltip: true,
-  legend: {
-    dataKey: 'country',
-    onItemMouseEnter: (ev) => {
-      // console.log(5, ev);
-    }
-  },
   series: [{
-    quickType: 'stackBar',
-    style: {
-      stroke: '#fff',
-      lineWidth: 1
-    },
-    position: 'year*percent',
-    color: 'country',
-    onMouseenter: (ev) => {
-      // console.log(3, ev);
-    },
-  }],
-  guide: [{
-    type: 'line',
-    top: true,
-    quickType: 'normal',
-    data: 'median',
-    lineStyle: {
-      stroke: '#ff6a00', // 线的颜色
-      lineWidth: 2 // 线的宽度
-    },
-    text: {
-      content: '水平线',
-      position: 'start',
-      autoRotate: false,
-      style: {
-        fill: '#ff6a00',
-      }
-    },
-    onMouseenter: (ev) => {
-      // console.log(2, ev);
-    },
-  }, {
-    type: 'line',
-    top: true,
-    quickType: 'parallel',
-    data: 'median',
-    lineStyle: {
-      stroke: '#ff6a00',
-      lineWidth: 2
-    },
-    text: {
-      content: '水平线',
-      position: 'start',
-      autoRotate: false,
-      style: {
-        fill: '#ff6a00',
-      }
-    },
-    onMouseenter: (ev) => {
-      // console.log(2, ev);
-    },
+    quickType: 'interval',
+    position: 'release*count',
+    color: '#e50000',
   }],
   brush: {
     type: 'X',
-    onBrushstart(chart) {
+    onBrushstart(ev, chart) {
       chart.hideTooltip();
     },
-    onBrushmove(chart) {
+    onBrushmove(ev, chart) {
       chart.hideTooltip();
     }
   },
@@ -90,8 +35,9 @@ viser({
     container: 'mount',
     forceFit: true,
     height: 400,
-    onDblClick: (ev) => {
-      // console.log(1, ev)
+    onPlotdblclick: (ev: any, chart: any) => {
+      chart.get('options').filters = {};
+      chart.repaint();
     }
   },
 });
