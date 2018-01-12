b62930886721026ba87dfced4eeb4a48e73967db
Ludy Hu <huxiaoyunhust@gmail.com>
2018-01-12 17:51:36 +0800

    feat: module.ts export Interval component (#97)

--- packages/viser-ng/src/module.ts
+++ packages/viser-ng/src/module.ts
@@ -2,7 +2,7 @@ import { NgModule, enableProdMode } from '@angular/core';
 import { Chart } from './Chart';
 import { LiteChart } from './LiteChart';
 import { Axis, Brush, Coord, Facet, Guide, Legend, Tooltip, View, FacetView, Series, Pie, Sector, Line, SmoothLine, DashLine, Area, StackArea, SmoothArea,
- Bar, StackBar, DodgeBar, Point, Funnel, Pyramid, Schema, Box, Candle, Polygon, Contour, Heatmap, Edge, Sankey, ErrorBar, JitterPoint, StackInterval } from './components/index';
+ Bar, StackBar, DodgeBar, Point, Funnel, Pyramid, Schema, Box, Candle, Polygon, Contour, Heatmap, Edge, Sankey, ErrorBar, JitterPoint, StackInterval, Interval } from './components/index';
 import * as viser from 'viser';
 
 @NgModule({
@@ -44,6 +44,7 @@ import * as viser from 'viser';
     ErrorBar,
     JitterPoint,
     StackInterval,
+    Interval,
   ],
   exports: [
     Chart,
@@ -83,6 +84,7 @@ import * as viser from 'viser';
     ErrorBar,
     JitterPoint,
     StackInterval,
+    Interval,
   ],
 })
 export class ViserModule {
