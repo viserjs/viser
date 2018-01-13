1645f4a41c353d657ac5f06d75ca865f1b608ddc
arcthur <arthurtemptation@gmail.com>
2018-01-13 12:08:57 +0800

    fix: remove unused package

--- packages/viser-react/src/components/LiteChart.tsx
+++ packages/viser-react/src/components/LiteChart.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as PropTypes from 'prop-types';
 import IRLiteChart from '../typed/IRLiteChart';
 import viser from 'viser';
 
