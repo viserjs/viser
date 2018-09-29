89944defe0bc71e95f14e7ebb5a4009c3b869557
ludy.hxy <huxiaoyunhust@gmail.com>
2018-09-29 17:19:58 +0800

    fix: remove some examples

--- demo/chart/index.ts
+++ demo/chart/index.ts
@@ -4,67 +4,11 @@ export default [
     case: ['json', 'react', 'vue', 'angular']
   },
   {
-    type: 'facet-1',
-    case: ['json', 'react', 'vue', 'angular']
-  },
-  {
-    type: 'sankey-1',
-    case: ['json', 'react', 'vue', 'angular']
-  },
-  {
-    type: 'chord-1',
-    case: ['json', 'react', 'vue', 'angular']
-  },
-  {
-    type: 'guage-1',
-    case: ['react', 'vue', 'angular']
-  },
-  {
-    type: 'map-1',
-    case: ['json']
-  },
-  {
-    type: 'stock-1',
-    case: ['json', 'react', 'vue', 'angular']
-  },
-  {
-    type: 'others-1',
-    case: ['json', 'react', 'vue', 'angular']
-  },
-  {
-    type: 'waffle',
-    case: ['react', 'vue', 'angular']
-  },
-  {
     type: 'cell-bar-1',
     case: ['json', 'vueCell']
   },
   {
-    type: 'graph',
-    case: ['json', 'react', 'angular', 'vueGraph']
-  },
-  {
-    type: 'tree',
-    case: ['json', 'react', 'angular', 'vueGraph']
-  },
-  {
-    type: 'compactBoxTree',
-    case: ['json', 'react', 'angular', 'vueGraph']
-  },
-  {
-    type: 'dendrogram',
-    case: ['json', 'react', 'angular', 'vueGraph']
-  },
-  {
-    type: 'mindmap',
-    case: ['json', 'react', 'angular', 'vueGraph']
-  },
-  {
-    type: 'functionTree',
-    case: ['json', 'react', 'angular', 'vueGraph']
-  },
-  {
-    type: 'indentedTree',
-    case: ['json', 'react', 'angular', 'vueGraph']
+    type: 'facet-1',
+    case: ['json', 'react', 'vue', 'angular']
   },
 ];
