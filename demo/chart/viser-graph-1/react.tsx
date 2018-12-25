de284305c5233dc513aa2010e622135a477c2b68
ludy.hxy <huxiaoyunhust@163.com>
2018-12-25 16:48:59 +0800

    fix: viser-graph node label bugfix and add viser-graph demo

---
+++ demo/chart/viser-graph-1/react.tsx
@@ -0,0 +1,198 @@
+import { Graph, Node, Edge, registerNode, registerEdge, Layouts, Util } from '../../../packages/viser-graph-react/src';
+import * as React from 'react';
+const data = {
+  "name": "Modeling Methods",
+  "children": [
+    {
+      "name": "Classification",
+      "children": [
+        {
+          "name": "Logistic regression"
+        },
+        {
+          "name": "Linear discriminant analysis"
+        },
+        {
+          "name": "Rules"
+        },
+        {
+          "name": "Decision trees"
+        },
+        {
+          "name": "Naive Bayes"
+        },
+        {
+          "name": "K nearest neighbor"
+        },
+        {
+          "name": "Probabilistic neural network"
+        },
+        {
+          "name": "Support vector machine"
+        }
+      ]
+    },
+    {
+      "name": "Consensus",
+      "children": [
+        {
+          "name": "Models diversity",
+          "children": [
+            {
+              "name": "Different initializations"
+            },
+            {
+              "name": "Different parameter choi9999999999999999999ces"
+            },
+            {
+              "name": "Different architectures"
+            },
+            {
+              "name": "Different modeling methods"
+            },
+            {
+              "name": "Different training sets"
+            },
+            {
+              "name": "Different feature sets"
+            }
+          ]
+        },
+        {
+          "name": "Methods",
+          "children": [
+            {
+              "name": "Classifier selection"
+            },
+            {
+              "name": "Classifier fusion"
+            }
+          ]
+        },
+        {
+          "name": "Common",
+          "children": [
+            {
+              "name": "Bagging"
+            },
+            {
+              "name": "Boosting"
+            },
+            {
+              "name": "AdaBoost"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Regression",
+      "children": [
+        {
+          "name": "Multiple linear regression"
+        },
+        {
+          "name": "Partial least squares"
+        },
+        {
+          "name": "Multi-layer feedforward neural network"
+        },
+        {
+          "name": "General regression neural network"
+        },
+        {
+          "name": "Support vector regression"
+        }
+      ]
+    }
+  ]
+};
+
+registerNode('treeNode', {
+  anchor: [[0, 0.5], [1, 0.5]]
+});
+
+registerEdge('smooth', {
+  getPath: function getPath(item) {
+    var points = item.getPoints();
+    var start = points[0];
+    var end = points[points.length - 1];
+    var hgap = Math.abs(end.x - start.x);
+    if (end.x > start.x) {
+      return [['M', start.x, start.y], ['C', start.x + hgap / 4, start.y, end.x - hgap / 2, end.y, end.x, end.y]];
+    }
+    return [['M', start.x, start.y], ['C', start.x - hgap / 4, start.y, end.x + hgap / 2, end.y, end.x, end.y]];
+  }
+});
+
+var layout = new Layouts.CompactBoxTree({
+  // direction: 'LR', // 方向（LR/RL/H/TB/BT/V）
+  getHGap: function getHGap() /* d */ {
+    // 横向间距
+    return 100;
+  },
+  getVGap: function getVGap() /* d */ {
+    // 竖向间距
+    return 10;
+  }
+});
+
+const graph = {
+  container: 'mount',
+  width: 500,
+  height: 500,
+  fitView: 'autoZoom',
+  fitViewPadding: true,
+  animate: true,
+  type: 'tree',
+  layout: layout,
+  data: {
+    roots: [data]
+  },
+  onAfterchange: function (ev, graph) {
+    console.log('onAfterchange')
+    graph.getNodes().forEach(function (node) {
+      var model = node.getModel();
+      var label = node.getLabel();
+      var keyShape = node.getKeyShape();
+      var children = node.getChildren();
+      var parent = node.getParent();
+      var box = keyShape.getBBox();
+      var labelBox = label.getBBox();
+      var dx = (box.maxX - box.minX + labelBox.maxX - labelBox.minX) / 2 + 8;
+      var dy = 0;
+      if (children.length != 0) {
+        dx = -dx;
+      }
+      label.translate(dx, dy);
+    });
+    graph.draw();
+  }
+};
+
+const node = {
+  shape: 'treeNode',
+  size: 8,
+  label: function (obj) {
+    return obj.name;
+  },
+};
+const edge = {
+  shape: 'smooth'
+};
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Graph {...graph}>
+          <Node {...node} />
+          <Edge {...edge} />
+        </Graph>
+      </div>
+    );
+  }
+}
