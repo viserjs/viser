34599e75f6e6042c834276afd4508118f04fd179
ludy.hxy <huxiaoyunhust@163.com>
2018-12-27 11:14:40 +0800

    fix: remove track function

--- packages/viser/src/core/CommonChart.ts
+++ packages/viser/src/core/CommonChart.ts
@@ -20,7 +20,7 @@ const G2 = require('@antv/g2');
 const Brush = require('@antv/g2-brush');
 
 // Disable G2 Track
-G2.track(false);
+// G2.track(false);
 
 function firstUpperCase(str: string) {
   return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
