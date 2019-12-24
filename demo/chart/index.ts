65c108da9ad11c9b6bcf55e22f020f5135bcc886
ludy.hxy <ludy.hxy@alibaba-inc.com>
2019-12-24 20:33:01 +0800

    fix: 注释demo

--- demo/chart/index.ts
+++ demo/chart/index.ts
@@ -3,217 +3,217 @@ export default [
     type: 'bar-1',
     case: ['json', 'react', 'vueGraph', 'angular'],
   },
-  {
-    type: 'cell-bar-1',
-    case: ['json', 'vueCell'],
-  },
-  {
-    type: 'facet-1',
-    case: ['json', 'react', 'vue', 'angular'],
-  },
-  {
-    type: 'viser-graph-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'slider',
-    case: ['json', 'react', 'vue', 'angular'],
-  },
-  // tree graph
-  {
-    type: 'CompactBox-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CompactBox-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Dendrogram-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Dendrogram-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Mindmap-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Mindmap-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Mindmap-3',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Mindmap-4',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Indented-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Indented-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'RadialTree-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'RadialTree-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CustomTreeItems-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CustomTreeItems-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  // general graph
-  {
-    type: 'ForceDirectedLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'ForceDirectedLayout-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'ForceDirectedLayout-3',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'ForceDirectedLayout-4',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'FruchtermanLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'FruchtermanLayout-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Grid-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Grid-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'DagreLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'DagreLayout-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'RadialLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'RadialLayout-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'RadialLayout-3',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CircularLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CircularLayout-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CircularLayout-3',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'CircularLayout-4',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'ConcentricLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'MDSLayout-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  // shape
-  {
-    type: 'Built-in Nodes-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-3',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-4',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-5',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-6',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-7',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Nodes-8',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-1',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-2',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-3',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-4',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-5',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-6',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
-  {
-    type: 'Built-in Edges-7',
-    case: ['json', 'react', 'vueGraph', 'angular'],
-  },
+  // {
+  //   type: 'cell-bar-1',
+  //   case: ['json', 'vueCell'],
+  // },
+  // {
+  //   type: 'facet-1',
+  //   case: ['json', 'react', 'vue', 'angular'],
+  // },
+  // {
+  //   type: 'viser-graph-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'slider',
+  //   case: ['json', 'react', 'vue', 'angular'],
+  // },
+  // // tree graph
+  // {
+  //   type: 'CompactBox-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CompactBox-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Dendrogram-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Dendrogram-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Mindmap-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Mindmap-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Mindmap-3',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Mindmap-4',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Indented-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Indented-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'RadialTree-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'RadialTree-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CustomTreeItems-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CustomTreeItems-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // // general graph
+  // {
+  //   type: 'ForceDirectedLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'ForceDirectedLayout-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'ForceDirectedLayout-3',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'ForceDirectedLayout-4',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'FruchtermanLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'FruchtermanLayout-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Grid-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Grid-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'DagreLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'DagreLayout-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'RadialLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'RadialLayout-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'RadialLayout-3',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CircularLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CircularLayout-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CircularLayout-3',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'CircularLayout-4',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'ConcentricLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'MDSLayout-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // // shape
+  // {
+  //   type: 'Built-in Nodes-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-3',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-4',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-5',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-6',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-7',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Nodes-8',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-1',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-2',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-3',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-4',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-5',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-6',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
+  // {
+  //   type: 'Built-in Edges-7',
+  //   case: ['json', 'react', 'vueGraph', 'angular'],
+  // },
   // {
   //   type: 'Custom Node-1',
   //   case: ['json', 'react', 'vueGraph', 'angular'],
